4bf3adf1a16249990aa11c298dc357d45dc83465
Your Name <yourgithubemail@gmail.com>
2026-01-06 19:54:57 +0530

    Fix CORS for frontend integration

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,16 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://grampanchayat-kolgiri.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
